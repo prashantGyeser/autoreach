bde336f8cbb58b6bb5d289731c1d6f873850c51b
Prashant <prashant@urbanzeak.com>
2015-08-04 04:57:37 -0700

    Changed the way the edit keyword works

--- app/controllers/dashboard/archive_controller.rb
+++ app/controllers/dashboard/archive_controller.rb
@@ -1,4 +1,5 @@
 class Dashboard::ArchiveController < ApplicationController
   def index
+    @articles = Article.where(user_id: current_user.id).where(irrelevant: false).where.not(facebook_shares: nil).where("facebook_shares > 0").order('keyword DESC').order( 'facebook_shares DESC' ).page(params[:page])
   end
 end
