0c7bfb537a6ad020956963cdaeb0aada4a15a6d9
Prashant <prashant@urbanzeak.com>
2015-08-05 03:35:36 -0700

    Before changing the name of the keyword column in the articles list

--- app/controllers/dashboard/archive_controller.rb
+++ app/controllers/dashboard/archive_controller.rb
@@ -1,5 +1,6 @@
 class Dashboard::ArchiveController < ApplicationController
   def index
-    @articles = Article.where(user_id: current_user.id).where(irrelevant: false).where.not(facebook_shares: nil).where("facebook_shares > 0").order('keyword DESC').order( 'facebook_shares DESC' ).page(params[:page])
+    
+    #@articles = Article.where(user_id: current_user.id).where(irrelevant: false).where.not(facebook_shares: nil).where("facebook_shares > 0").order('keyword DESC').order( 'facebook_shares DESC' ).page(params[:page])
   end
 end
