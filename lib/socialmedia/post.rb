022651beb89ac12e3971bfd3e7bafa03aba84564
Prashant <prashant@urbanzeak.com>
2015-08-11 20:27:13 -0700

    Updated the message that will be posted to twitter. Still need to test it out and see if it is all okay

--- lib/socialmedia/post.rb
+++ lib/socialmedia/post.rb
@@ -1,8 +1,8 @@
 class Post
 
-  def twitter(twitter_credentials, url)
+  def twitter(twitter_credentials, args)
     twitter_client = TwitterClient.initialize_client(twitter_credentials)
-    twitter_client.update(generate_twitter_message(url))
+    twitter_client.update(generate_twitter_message(args[:url], args[:title], args[:hashtag]))
   end
 
   def facebook(token, message)
@@ -10,9 +10,9 @@ class Post
   end
 
   private
-  def generate_twitter_message(url)
-    url_summary = Summary.new.url(url)
-    url_summary.truncate(summary_length_twitter(url)) + ' ' + url
+  def generate_twitter_message(url, title, hashtag)
+    message = message(title, hashtag)
+    message.truncate(summary_length_twitter(url)) + ' ' + url
   end
 
   def summary_length_twitter(url)
@@ -20,6 +20,10 @@ class Post
     # A message can be a maximum of 140 characters in length
     # Setting it to 125 here to account for any additional spaces that might be added in the message
     return (125 - url_length)
+  end
+
+  def message(title, hashtag)
+    hashtag + ' ' + title
   end
 
 end
