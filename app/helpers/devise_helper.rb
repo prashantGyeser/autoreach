ba1f7af038b5eed63e4af10f292c8e64df302bba
Prashant <prashant@urbanzeak.com>
2015-09-02 20:11:59 -0700

    Added an alert message for the devise login page

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,15 +1,15 @@
 module DeviseHelper
   def devise_error_messages!
-    return "" if resource.errors.empty?
+    return '' if resource.errors.empty?
+
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-
-    <div class="alert alert-danger" role="alert">
-      <button class="close" data-dismiss="alert"></button>
-        <strong>Error </strong>#{messages}
+    <div class="alert alert-error alert-danger"> <button type="button"
+    class="close" data-dismiss="alert">x</button>
+      #{messages}
     </div>
-
     HTML
+
     html.html_safe
   end
 end
