9587de21cc90476a6c4c54d0d5f24e1f4f9ddc08
Prashant <prashant@urbanzeak.com>
2015-09-06 06:27:07 -0700

    Setup a basic integration test for sign up. Still need to add code to show a progress indicator for the articles as the search is happening and need to add integration tests for the entire application

--- spec/features/devise/sign_up_spec.rb
+++ spec/features/devise/sign_up_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 RSpec.feature "Signup", type: :feature do
   scenario "user signs up" do
     visit new_user_registration_path
+
+    fill_in "user_email", with: "test@test.com"
+    fill_in "user_password", with: "password@123"
+    fill_in "user_password_confirmation", with: "password@123"
+
+    click_button "Sign up"
+
+    expect(page).to have_content("SETUP KEYWORD")
+
   end
 end
 
